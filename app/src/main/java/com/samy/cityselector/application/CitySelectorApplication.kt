ba1622a4939763d0fd4514de6be352481263218c
Saamyyy <Mahmoud.afify@talabat.com>
2021-08-31 21:51:40 +0400

    create application class to set up koin

---
+++ app/src/main/java/com/samy/cityselector/application/CitySelectorApplication.kt
@@ -0,0 +1,21 @@
+package com.samy.cityselector.application
+
+import android.app.Application
+import com.samy.cityselector.di.dataModule
+import com.samy.cityselector.di.domainModule
+import com.samy.cityselector.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class CitySelectorApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Start Koin
+        startKoin {
+            androidLogger()
+            androidContext(this@CitySelectorApplication)
+            modules(listOf(dataModule, domainModule, viewModelModule))
+        }
+    }
+}
