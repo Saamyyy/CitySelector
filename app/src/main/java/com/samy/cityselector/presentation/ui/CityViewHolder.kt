c89dd5ed1e44dd6cb27f8e9c063999df99533d9a
Saamyyy <Mahmoud.afify@talabat.com>
2021-09-03 15:41:55 +0400

    add google maps view along with its configurations

--- app/src/main/java/com/samy/cityselector/presentation/ui/CityViewHolder.kt
+++ app/src/main/java/com/samy/cityselector/presentation/ui/CityViewHolder.kt
@@ -3,6 +3,8 @@ package com.samy.cityselector.presentation.ui
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.samy.cityselector.R
 import com.samy.cityselector.presentation.entities.CityViewEntityItem
@@ -10,11 +12,21 @@ import com.samy.cityselector.presentation.entities.CityViewEntityItem
 class CityViewHolder(parentView: ViewGroup) : RecyclerView.ViewHolder(
     View.inflate(parentView.context, R.layout.city_list_item, null)
 ) {
+    private val cityItemContainer: ConstraintLayout by lazy { itemView.findViewById(R.id.cityItemContainer) }
     private val tittle: TextView by lazy { itemView.findViewById(R.id.cityName) }
     private val supTitle: TextView by lazy { itemView.findViewById(R.id.cityLocation) }
 
     fun bind(cityViewEntityItem: CityViewEntityItem) {
         tittle.text = cityViewEntityItem.title
         supTitle.text = cityViewEntityItem.supTitle
+        cityItemContainer.setOnClickListener {
+            val action = CityListFragmentDirections.actionCityListFragmentToCityMapFragment(
+                lat = cityViewEntityItem.lat,
+                lon = cityViewEntityItem.lon,
+                cityName = cityViewEntityItem.title
+            )
+
+            itemView.findNavController().navigate(action)
+        }
     }
 }
---
+++ app/src/main/java/com/samy/cityselector/presentation/ui/citymap/CityMapFragment.kt
@@ -0,0 +1,40 @@
+package com.samy.cityselector.presentation.ui.citymap
+
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.samy.cityselector.R
+
+class CityMapFragment : Fragment() {
+    private val args: CityMapFragmentArgs by navArgs()
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        val sydney = LatLng(args.lat.toDouble(), args.lon.toDouble())
+        googleMap.addMarker(MarkerOptions().position(sydney).title(args.cityName))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_city_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
